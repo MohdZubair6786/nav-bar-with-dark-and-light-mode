160c94b535136bc762f169d060833989d5745ee1
Mohd Zubair <mohmmedzubair6786@gail.com>
2024-08-22 23:21:35 +0530

    created proroject

---
+++ script.js
@@ -0,0 +1,12 @@
+const body = document.getElementsByTagName("body")[0];
+const para = document.getElementById("para");
+
+function lightbutton(){
+    body.style.backgroundColor="white";
+    para.style.color= "black";
+    
+}
+function darkbutton(){
+    body.style.backgroundColor = "black"
+    para.style.color= "white";
+}
